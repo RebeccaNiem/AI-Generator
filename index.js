82ba447af21d91e0171a8c54b79b6083c91f1d70
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-04-26 14:51:38 +0200

    changed h1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function generateTextFunction(event) {
   let instructionsElement = document.querySelector("#instructions");
   let apiKey = "280o02ba0daf2b414a53ctfe4e6155a2";
   let context =
-    "you are an expert in everything. You love giving smart and short answers and you are very friendly. Make sure to answer precisely to the user's questions. Make a paragraph and sign the answer with `Your Ai-Assistant` inside a <strong> element at the end of the answer.";
+    "you are an expert in everything. You love giving smart and very short answers and you are very friendly. Make sure to answer precisely to the user's questions. Please make a paragraph after your answer. Under the paragraph please sign the answer with `Your Ai-Assistant` inside a <strong> element .";
   let prompt = `User's question: Generate an answer about ${instructionsElement.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
