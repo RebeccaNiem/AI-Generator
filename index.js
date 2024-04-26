6c51fbad6eb405c022d33482f34b5d21f88dae4a
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-04-26 14:40:24 +0200

    added the AI API

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function generateTextFunction(event) {
-  event.preventDefault();
-
-  let TextElement = document.querySelector("#text");
-
+function displayText(response) {
   new Typewriter("#text", {
-    strings: ["here is your answer"],
+    strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateTextFunction(event) {
+  event.preventDefault();
+
+  let instructionsElement = document.querySelector("#instructions");
+  let apiKey = "280o02ba0daf2b414a53ctfe4e6155a2";
+  let context =
+    "you are an expert in everything. You love giving smart and short answers and you are very friendly. Make sure to answer precisely to the user's questions. Make a paragraph and sign the answer with `Your Ai-Assistant` inside a <strong> element at the end of the answer.";
+  let prompt = `User's question: Generate an answer about ${instructionsElement.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayText);
 }
 
 let generateText = document.querySelector("#textGenerator");
