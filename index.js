f649bf01a58475c9b489c8188761fa010dd20233
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-04-26 12:58:12 +0200

    added basic javascript

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hi!");
+function generateTextFunction(event) {
+  event.preventDefault();
+
+  let TextElement = document.querySelector("#text");
+
+  new Typewriter("#text", {
+    strings: ["here is your answer"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let generateText = document.querySelector("#textGenerator");
+generateText.addEventListener("submit", generateTextFunction);
