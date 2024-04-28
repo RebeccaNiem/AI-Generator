a7beab4cb92d0272d4529a64360872e3f7da6464
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-04-28 22:55:24 +0200

    added some CSS and UX

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ function generateTextFunction(event) {
   let instructionsElement = document.querySelector("#instructions");
   let apiKey = "280o02ba0daf2b414a53ctfe4e6155a2";
   let context =
-    "you are an expert in everything. You love giving smart and very short answers and you are very friendly. Please try to answer on follow up questions and provide an other answer. Make sure to answer precisely to the user's questions. Please make a paragraph after your answer. Under the paragraph please sign the answer with `Your Ai-Assistant` inside a <strong> element .";
+    "you are an expert in everything. You love giving smart and very short answers and you are very friendly. You like to to use smileys.Please sign the answer with `Your Ai-Assistant🫡` inside a <strong> element .";
   let prompt = `User's question: Generate an answer about ${instructionsElement.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let textElement = document.querySelector("#text");
+  textElement.classList.remove("hidden");
+  textElement.innerHTML = `<div class="generating">⏳ Generating an answer about ${instructionsElement.value}</div>`;
 
   axios.get(apiURL).then(displayText);
 }
